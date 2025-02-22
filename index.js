ee142343d8645b4dc4612252ff9641608507885f
Jugesh-Pawase <jugeshpawase@gmail.com>
2025-02-23 01:20:31 +0530

    Update index.js added *

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "http://localhost:3000",   //Frontend request
+        origin: "*",  //Frontend request
         credentials: true,
     })
 );
